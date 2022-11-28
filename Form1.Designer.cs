80387853fdf7d73b38b1a19c2faffab808d8f1f9
Tanix <txlu@mail.ustc.edu.cn>
2022-11-29 02:11:16 +0800

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 553F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(982, 553);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(461, 553);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(519, 0);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(463, 553);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.WordWrap = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(461, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(58, 553);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "→";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(0, 358);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "←";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Easy Safe Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace EasySafeChat
 {
     public partial class Form1 : Form
     {
+        private static readonly ECCurve CURVE = ECCurve.NamedCurves.nistP521;
+        // 密钥存储格式：第一行X，第二行Y（X和Y组成公钥），第三行D（私钥，可不填）
+        private const string MY_KEYS_PATH = "MyKeys.txt";
+        private const string OTHERS_PUBLIC_KEY_PATH = "OthersPublicKey.txt";
+        private static ECDsa MyEcdsa { get; set; }
+        private static ECDsa OthersEcdsa { get; set; }
+
+        private static ECDsa LoadEcdsaFromHex(string xHex, string yHex, string privateHex = null)
+        {
+            try
+            {
+                return ECDsa.Create(new ECParameters
+                {
+                    Curve = CURVE,
+                    Q = new ECPoint
+                    {
+                        X = HexToByteArray(xHex),
+                        Y = HexToByteArray(yHex)
+                    },
+                    D = HexToByteArray(privateHex)
+                });
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveEcdsa2File(string path, ECDsa ecdsa)
+        {
+            var xHex = ByteArrayToHex(ecdsa.ExportParameters(false).Q.X);
+            var yHex = ByteArrayToHex(ecdsa.ExportParameters(false).Q.Y);
+            string[] lines;
+            try
+            {
+                var dHex = ByteArrayToHex(ecdsa.ExportParameters(true).D);
+                lines = new string[] { xHex, yHex, dHex };
+            }
+            catch (CryptographicException)
+            {
+                lines = new string[] { xHex, yHex };
+            }
+            File.WriteAllLines(path, lines);
+        }
+
+        private static ECDsa LoadEcdsaFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            if (lines.Length < 2) return null;
+            else if (lines.Length == 2) return LoadEcdsaFromHex(lines[0].Trim(), lines[1].Trim());
+            else return LoadEcdsaFromHex(lines[0].Trim(), lines[1].Trim(), lines[2].Trim());
+        }
+
+        private static byte[] HexToByteArray(string hex)
+        {
+            if (hex is null) return null;
+            else
+            {
+                int numberChars = hex.Length;
+                byte[] bytes = new byte[numberChars / 2];
+                for (int i = 0; i < numberChars; i += 2)
+                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                return bytes;
+            }
+        }
+
+        public static string ByteArrayToHex(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", "");
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(MY_KEYS_PATH) && new FileInfo(MY_KEYS_PATH).Length != 0)
+            {
+                MyEcdsa = LoadEcdsaFromFile(MY_KEYS_PATH);
+                if (MyEcdsa is null) MessageBox.Show(
+                    $"文件{MY_KEYS_PATH}格式错误，请将其修复或清空",
+                    "错误",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+            }
+            else
+            {
+                MyEcdsa = ECDsa.Create(CURVE);
+                SaveEcdsa2File(MY_KEYS_PATH, MyEcdsa);
+                MessageBox.Show(
+                    $"成功生成密钥！已保存至{MY_KEYS_PATH}",
+                    "提示",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information
+                );
+            }
+            if (File.Exists(OTHERS_PUBLIC_KEY_PATH) && new FileInfo(OTHERS_PUBLIC_KEY_PATH).Length != 0)
+            {
+                OthersEcdsa = LoadEcdsaFromFile(OTHERS_PUBLIC_KEY_PATH);
+                if (OthersEcdsa is null) MessageBox.Show(
+                    $"文件{OTHERS_PUBLIC_KEY_PATH}格式错误，请修复或将其清空",
+                    "错误",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text
         }
     }
 }
